d060084ec6974401457320952951b00823d822b6
levi <cgfeel@gmail.com>
2026-07-13 00:56:23 +0800

    fix: update cli provider default command

--- packages/micro-dev-config/src/index.ts
+++ packages/micro-dev-config/src/index.ts
@@ -1,16 +1,19 @@
 import build from './commands/build.ts'
 import serve from './commands/serve.ts'
 
+const COMMANDS = ['build', 'serve']
 const args = process.argv.slice(2)
-const command = args[0]
+
+const command = COMMANDS.includes(args[0]) ? args[0] : 'build'
+const commandArgs = COMMANDS.includes(args[0]) ? args.slice(1) : args
 
 async function main() {
   switch (command) {
     case 'build':
-      await build(args.slice(1))
+      await build(commandArgs)
       break
     case 'serve':
-      await serve(args.slice(1))
+      await serve(commandArgs)
       break
     default:
       console.log(`
