7d57e20bc84debc3ae71c3e27e87334ad889b375
levi <cgfeel@gmail.com>
2026-07-13 22:16:38 +0800

    fix: split plugin

--- packages/micro-dev-config/src/helpers.ts
+++ packages/micro-dev-config/src/helpers.ts
@@ -1,25 +1,6 @@
 import { createRequire } from 'node:module'
-import CopyPlugin from 'copy-webpack-plugin'
-import webpack from 'webpack'
 
 const requireFromCLI = createRequire(import.meta.url)
-
-export const HtmlWebpackPlugin = requireFromCLI('html-webpack-plugin')
-export function copyPlugin(patterns: CopyPlugin.Pattern[]) {
-  return new CopyPlugin({ patterns })
-}
-
-export function defineEnvPlugin(env: PluginEnvType, defineRecord: PluginDefineType = {}) {
-  return new webpack.DefinePlugin({
-    ...Object.fromEntries(
-      Object.entries(defineRecord).map(([key, value]) => [
-        `process.env.${key}`,
-        JSON.stringify(value),
-      ])
-    ),
-    'process.env.NODE_ENV': JSON.stringify(env.production ? 'production' : 'development'),
-  })
-}
 
 /**
  * 返回 loader 的绝对路径
@@ -41,6 +22,3 @@ export function plugin<T>(name: string): T {
 export function resolve(name: string) {
   return requireFromCLI.resolve(name)
 }
-
-type PluginEnvType = { production?: boolean }
-type PluginDefineType = Record<string, unknown>
