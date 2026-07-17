7ec8b0b689244d26aedfeb832171d09a374d730e
levi <cgfeel@gmail.com>
2026-07-18 04:40:50 +0800

    fix: reduce Promise.resolve

--- packages/single-spa/src/lifecycles/load.ts
+++ packages/single-spa/src/lifecycles/load.ts
@@ -1,30 +1,28 @@
 import { APPLICATION_STATUS, AppItemType, MountActionType } from '../application/app.helpers.ts'
 
 export function toLoadPromise(app: AppItemType) {
-  return Promise.resolve().then(() => {
-    const { customProps, status, loadApp, trigger } = app
-    if (status !== APPLICATION_STATUS.NOT_LOADED) {
-      // 此应用已加载完毕了
+  const { customProps, status, loadApp, trigger } = app
+  if (status !== APPLICATION_STATUS.NOT_LOADED) {
+    // 此应用已加载完毕了
+    return app
+  }
+
+  // 正在加载应用
+  app.status = APPLICATION_STATUS.LOADING_SOURCE_CODE
+
+  // 相当于 systemjs 中的 System.import
+  return Promise.resolve()
+    .then(() => loadApp(customProps))
+    .then(({ bootstrap, mount, unmount }) => {
+      // 将加载的信息更新到 app 并返回
+      app.status = APPLICATION_STATUS.NOT_BOOTSTRAPED
+      app.bootstrap = flattenArrayToPromise(bootstrap)
+      app.mount = flattenArrayToPromise(mount)
+      app.unmount = flattenArrayToPromise(unmount)
+
       return app
-    }
-
-    // 正在加载应用
-    app.status = APPLICATION_STATUS.LOADING_SOURCE_CODE
-
-    // 相当于 systemjs 中的 System.import
-    return Promise.resolve()
-      .then(() => loadApp(customProps))
-      .then(({ bootstrap, mount, unmount }) => {
-        // 将加载的信息更新到 app 并返回
-        app.status = APPLICATION_STATUS.NOT_BOOTSTRAPED
-        app.bootstrap = flattenArrayToPromise(bootstrap)
-        app.mount = flattenArrayToPromise(mount)
-        app.unmount = flattenArrayToPromise(unmount)
-
-        return app
-      })
-      .finally(() => trigger())
-  })
+    })
+    .finally(() => trigger())
 }
 
 // 拍平挂载的方法
